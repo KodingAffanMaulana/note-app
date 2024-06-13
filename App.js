7785999b195cdc33f620aa66f70acd0106be5795
Affan Maulana <affan.maulana2016@students.unila.ac.id>
2024-06-13 18:56:51 +0700

    feat: Add AddNote screen and components
    The code changes include adding the following files:
    - babel.config.js
    - .gitignore
    - package.json
    - app.json
    - components/customButton.js
    - components/customTextInput.js
    - screens/addNote.js
    
    These changes introduce the AddNote screen and its related components, including CustomButton and CustomTextInput. The AddNote screen allows users to add a new note with a title and description.

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Home from './screens/home';
+import AddNote from './screens/addNote';
+import EditNote from './screens/editNote';
+
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+  deleteNote,
+  editNote,
+  selectedNote,
+  setSelectedNote,
+}) => {
+  switch (currentPage) {
+    case 'home':
+      return (
+        <Home
+          noteList={noteList}
+          setCurrentPage={setCurrentPage}
+          deleteNote={deleteNote}
+          setSelectedNote={setSelectedNote}
+        />
+      );
+    case 'add':
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case 'edit':
+      return (
+        <EditNote setCurrentPage={setCurrentPage} editNote={editNote} selectedNote={selectedNote} />
+      );
+    default:
+      return <Home />;
+  }
+};
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home');
+  const [selectedNote, setSelectedNote] = useState(null);
+  const [noteList, setNoteList] = useState([
+    {
+      id: 1,
+      title: 'Note pertama',
+      desc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+    },
+  ]);
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title,
+        desc,
+      },
+    ]);
+  };
+
+  const deleteNote = (id) => {
+    setNoteList(noteList.filter((note) => note.id !== id));
+  };
+
+  const editNote = (id, title, desc) => {
+    setNoteList(noteList.map((note) => (note.id === id ? { id, title, desc } : note)));
+  };
+
+  return (
+    <CurrentPageWidget
+      currentPage={currentPage}
+      noteList={noteList}
+      setCurrentPage={setCurrentPage}
+      addNote={addNote}
+      deleteNote={deleteNote}
+      editNote={editNote}
+      selectedNote={selectedNote}
+      setSelectedNote={setSelectedNote}
+    />
+  );
+};
+
+export default App;
